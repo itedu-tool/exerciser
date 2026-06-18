ce5516e89ea814c6c0a9addb0425e0dc93aeb1bb
Starinin Andrey <starininandrey@gmail.com>
2026-06-18 10:40:58 +0300

    Добавление нового DTO, endpoint Изменение репозитория

--- Exerciser.WebApi/Repositories/AttemptRepository.cs
+++ Exerciser.WebApi/Repositories/AttemptRepository.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using Exerciser.WebApi.Models;
 
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Exerciser.WebApi.Repositories;
@@ -13,6 +15,7 @@ public interface IAttemptRepository
     Task<Attempt?> GetByIdAsync(Guid id);
     Task UpdateAsync(Attempt attempt);
     Task<Attempt?> GetLatestUnfinishedAsync(Guid sessionId, Guid examId);
+    Task<IEnumerable<Attempt>> GetLastFinishedAttemptsByStudentAndExamAsync();
 }
 
 public class AttemptRepository : IAttemptRepository
@@ -45,5 +48,33 @@ public class AttemptRepository : IAttemptRepository
         return await _attempts.Find(a => a.SessionId == sessionId && a.Exam.Id == examId && a.FinishedAt == null)
             .SortByDescending(a => a.StartedAt)
             .FirstOrDefaultAsync();
+    }
+    
+    public async Task<IEnumerable<Attempt>> GetLastFinishedAttemptsByStudentAndExamAsync()
+    {
+        var pipeline = new[]
+        {
+            // Фильтр: только завершённые попытки
+            new BsonDocument("$match", new BsonDocument("FinishedAt", new BsonDocument("$ne", BsonNull.Value))),
+            // Сортировка по убыванию даты завершения
+            new BsonDocument("$sort", new BsonDocument("FinishedAt", -1)),
+            // Группировка по студенту + группе + экзамену
+            new BsonDocument("$group", new BsonDocument
+            {
+                { "_id", new BsonDocument
+                    {
+                        { "studentName", "$Student.FullName" },
+                        { "groupName", "$Student.GroupName" },
+                        { "examId", "$Exam.Id" },
+                        { "examTitle", "$Exam.Title" }
+                    }
+                },
+                { "doc", new BsonDocument("$first", "$$ROOT") }
+            }),
+            // Замена на документ
+            new BsonDocument("$replaceRoot", new BsonDocument("newRoot", "$doc"))
+        };
+
+        return await _attempts.Aggregate<Attempt>(pipeline).ToListAsync();
     }
 }
